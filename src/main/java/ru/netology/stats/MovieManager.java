dad063a9c527b9cb6e7fd0d52d7bdbdb1bd0dfe3
rusLAN <puzo00712@gmail.com>
2023-08-16 15:15:11 +0300

    initial commit

--- src/main/java/ru/netology/stats/MovieManager.java
+++ src/main/java/ru/netology/stats/MovieManager.java
@@ -2,6 +2,15 @@ package ru.netology.stats;
 
 public class MovieManager {
     private String[] movies = new String[0];
+    private int limit;
+
+    public MovieManager(int limit) {
+        this.limit = limit;
+    }
+
+    public MovieManager() {
+        this.limit = 5;
+    }
 
     public void addNewMovie(String movie) {
         String[] tmp = new String[movies.length + 1];
@@ -17,7 +26,13 @@ public class MovieManager {
     }
 
     public String[] findLast() {
-        String[] tmp = new String[5];
+        int resultLength;
+        if (movies.length < limit) {
+            resultLength = movies.length;
+        } else {
+            resultLength = limit;
+        }
+        String[] tmp = new String[resultLength];
         for (int i = 0; i < tmp.length; i++) {
             tmp[i] = movies[movies.length - 1 - i];
         }
--- src/test/java/ru/netology/stats/MovieManagerTest.java
+++ src/test/java/ru/netology/stats/MovieManagerTest.java
@@ -4,10 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MovieManagerTest {
-    MovieManager manager = new MovieManager();
+    //MovieManager manager = new MovieManager();
 
     @Test
     public void addMovie() {
+        MovieManager manager = new MovieManager();
 
         manager.addNewMovie("Bloodshot");
         manager.addNewMovie("Forward");
@@ -23,6 +24,7 @@ public class MovieManagerTest {
 
     @Test
     public void latestMovies() {
+        MovieManager manager = new MovieManager();
 
         manager.addNewMovie("Bloodshot");
         manager.addNewMovie("Forward");
@@ -33,6 +35,36 @@ public class MovieManagerTest {
 
         String[] expected = {"Number one", "Trolls", "Invisible Man", "Gentlemen", "Forward"};
         String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void minMovies() {
+        MovieManager manager = new MovieManager();
+
+        manager.addNewMovie("Bloodshot");
+        manager.addNewMovie("Forward");
+        manager.addNewMovie("Gentlemen");
+
+        String[] expected = {"Gentlemen", "Forward", "Bloodshot"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void maxLimitMovies() {
+        MovieManager manager = new MovieManager(7);
+
+        manager.addNewMovie("Bloodshot");
+        manager.addNewMovie("Forward");
+        manager.addNewMovie("Gentlemen");
+        manager.addNewMovie("Invisible Man");
+        manager.addNewMovie("Trolls");
+        manager.addNewMovie("Number one");
+        manager.addNewMovie("Trap");
+
+        String[] expected = {"Bloodshot", "Forward", "Gentlemen", "Invisible Man", "Trolls", "Number one","Trap"};
+        String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
 }
